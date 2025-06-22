8fbb6ea88182bf0721c40dfe919728db36fc478b
bryanwaine <nwanne.ezeaka@gmail.com>
2025-06-22 04:30:59 +0100

    Add animation

--- index.js
+++ index.js
@@ -149,3 +149,21 @@ async function getMovie(page = 1) {
     console.log(`Error: ${error}`);
   }
 }
+
+const callback = (entries, observer) => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting) {
+      entry.target.classList.add('slide-up')
+      observer.unobserve(entry.target)
+    } 
+  })
+}
+
+const options = {
+  threshold: 0.2
+}
+
+const observer = new IntersectionObserver(callback, options)
+
+const animatedElements = document.querySelectorAll('.content')
+animatedElements.forEach(el => observer.observe(el))
--- watchlist.js
+++ watchlist.js
@@ -62,3 +62,21 @@ document.querySelectorAll(".remove-btn").forEach((btn) => {
     window.location.reload();
   });
 });
+
+const callback = (entries, observer) => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting) {
+      entry.target.classList.add('slide-up')
+      observer.unobserve(entry.target)
+    } 
+  })
+}
+
+const options = {
+  threshold: 0.2
+}
+
+const observer = new IntersectionObserver(callback, options)
+
+const animatedElements = document.querySelectorAll('.content')
+animatedElements.forEach(el => observer.observe(el))
